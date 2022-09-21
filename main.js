62c617260842bf356526c802d017b8eea162cabb
Sebastian Garcia <sfernandogarcia@uts.edu.co>
2022-09-21 12:42:15 -0500

    add filles

---
+++ main.js
@@ -0,0 +1,43 @@
+const convertir = document.getElementById('convertir');
+const temperatura = document.getElementById('temperatura');
+const tempError = document.getElementById('tempError');
+const termometro = document.getElementById('termometro');
+const marcador = document.getElementById('marcador');
+var confirmacion = false;
+temperatura.addEventListener('input', () => {
+  if(temperatura.value < 32) {
+    tempError.style.display = 'block';
+    tempError.textContent = 'La temperatura debe ser mayor a 32';
+    confirmacion = false;
+  } else if (temperatura.value > 212) {
+    tempError.style.display = 'block';
+    tempError.textContent = 'La temperatura debe ser menor a 212';
+    confirmacion = false;
+  }
+  else {
+    tempError.style.display = 'none';
+    confirmacion = true;
+  }
+});
+var porcentaje = 0;
+function progress(i) {
+    setTimeout(function() {
+      marcador.textContent = `${i}°C`;
+      marcador.style.height = `${i}%`;
+    }, i * 10);
+}
+
+convertir.addEventListener('click', () => {
+  const resultado = document.getElementById('resultado');
+
+  if(confirmacion) {
+    const celsius = ((temperatura.value - 32) * 5 / 9).toFixed(1);
+    porcentaje =  (celsius * 100 / 100).toFixed(1);
+    resultado.textContent = `${celsius} °C`;
+    for(let i = 0; i <=porcentaje; i++) {
+      progress(i);
+    }
+  }
+});
+
+// Language: javascript
